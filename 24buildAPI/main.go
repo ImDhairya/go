87304e0ce8460416254d134b8b0e7163dd02e108
ImDhairya <pandyadhairya789@gmail.com>
2025-09-08 07:56:39 +0530

    complete building courses api

--- 24buildAPI/main.go
+++ 24buildAPI/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 )
 
 // model for courses  - file
@@ -23,7 +27,7 @@ type Author struct {
 
 // fake DB
 
-var courser []Course
+var courses []Course
 
 // middlewares -- separate file
 
@@ -32,31 +36,118 @@ func (c *Course) IsEmpty() bool {
 }
 
 func main() {
+	courses = append(courses, Course{CourseId: "32", CourseName: "Golang course", CoursePrice: 32, Author: &Author{FullName: "Dhairya Pandya", Website: "strawbwerery.site"}})
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", serveHome).Methods("GET")
+	r.HandleFunc("/courses", getAllCourses).Methods("GET")
+	r.HandleFunc("/courses/{id}", getOneCourse).Methods("GET")
+	r.HandleFunc("/course", createOneCourse).Methods("POST")
+	r.HandleFunc("/course/{id}", updateOneCourse).Methods("PUT")
+	r.HandleFunc("/course/{id}", deleteOneCourse).Methods("DELETE")
+
+	fmt.Println("The server is listening at 4040")
+	log.Fatal(http.ListenAndServe(":4040", r))
+
+	// this is called seedint the database
+
 
 }
 
-// controllers - differnt files 
+// controllers - differnt files
 
 // serve home route '/'
 
-func serveHome(w  http.ResponseWriter , r *http.Request ) {
+func serveHome(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte("<h1> Welcome to api by Dhairya. </h1>"))
 }
 
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get all courses.")
 
-
-func getAllCourses(w http.ResponseWriter , r *http.Request) {
-	fmt.Println("Get all courses")
-
-	w.Header().Set("Content-Type", "applicatoin/json")
-	json.NewEncoder(w).Encode(courser)
+	w.Header().Set("Contnet-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
 
 }
 
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Getting one course")
+	w.Header().Set("Content-Type", "application/json")
+	// get the id
 
+	params := mux.Vars(r)
 
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
 
+			return
+		}
+	}
 
+	json.NewEncoder(w).Encode("No courses were found ")
+	return
+}
 
+func createOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create one course.")
 
+	w.Header().Set("Content-Type", "application/json")
+
+	// what if entire body is empty
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please send some data.")
+	}
+
+	var course Course
+
+	_ = json.NewDecoder(r.Body).Decode(&course)
+
+	if course.IsEmpty() {
+		json.NewEncoder(w).Encode("No data in file.")
+		return
+	}
+
+	// rand.Seed(time.Now().UnixNano())
+
+	course.CourseId = strconv.Itoa(rand.Intn(100))
+
+	courses = append(courses, course)
+
+	json.NewEncoder(w).Encode(courses)
+
+}
+func updateOneCourse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range courses {
+		if item.CourseId == params["id"] {
+			var updatedCourse Course
+			_ = json.NewDecoder(r.Body).Decode(&updatedCourse)
+
+			updatedCourse.CourseId = params["id"]
+			courses[index] = updatedCourse
+
+			json.NewEncoder(w).Encode(updatedCourse)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("Course not found")
+}
+
+func deleteOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Delete request received.")
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range courses {
+		if item.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			json.NewEncoder(w).Encode(courses)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("Course not found")
+}
